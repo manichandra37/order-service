aa7147ce5375088dbe2c13f3b1599ca27f9e9dcc
manichandra37 <manichandramaddi73@gmail.com>
2026-02-07 19:30:59 -0600

    first commit

---
+++ src/test/java/com/mani/commerce/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mani.commerce.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
